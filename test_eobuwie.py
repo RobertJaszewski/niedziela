28b6607866b0baf33e4766b4eeec6c30ff1e2a97
RobertJaszewski <RobertJaszewski@users.noreply.github.com>
2022-10-16 13:28:48 +0200

    dodano funkcjonalnosc obsluga chrome

--- test_eobuwie.py
+++ test_eobuwie.py
@@ -20,7 +20,6 @@ class RejestracjaNowegoUzytkownika(unittest.TestCase):
         # WARUNKI WSTĘPNE
         # 1. Otwarta przeglądarka
         #self.driver = webdriver.Chrome()
-        self.driver = webdriver.Remote(command_executor='http://127.0.0.1:4444/wd/hub', desired_capabilities=DesiredCapabilities.EDGE)
         self.driver.maximize_window()
         # 2. Otwarta strona główna
         self.driver.get("https://www.eobuwie.com.pl/")
